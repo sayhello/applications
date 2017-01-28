1f5224304d7636457ac77ae308d82ee49d76f15e
sayhello <sayhello@safe-mail.net>
2017-01-28 20:59:51 +0100

    inline and css

--- bereal/src/app/app.component.ts
+++ bereal/src/app/app.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `
+    <div class="app">
+      Hello! {{title}}
+    </div>
+  `
 })
 export class AppComponent {
   title = 'app works!';
